9ad12050e164500c8d3422b91931fcb361cd761d
akshay <fxn769@gmail.com>
2017-09-14 23:14:01 +0530

    Placeholder Fragment Added

---
+++ app/src/main/java/ak/sh/ay/app/PlaceholderFragment.java
@@ -0,0 +1,93 @@
+package ak.sh.ay.app;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import ak.sh.ay.oblique.GradientAngle;
+import ak.sh.ay.oblique.ObliqueView;
+import ak.sh.ay.oblique.Type;
+
+/**
+ * Created by akshay on 14/9/17.
+ */
+
+public class PlaceholderFragment extends Fragment {
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    public PlaceholderFragment() {
+    }
+
+    public static PlaceholderFragment newInstance(int sectionNumber) {
+        PlaceholderFragment fragment = new PlaceholderFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_sample2, container, false);
+        ObliqueView obliqueView = (ObliqueView) rootView.findViewById(R.id.obliqueView);
+        TextView text_oblique = (TextView) rootView.findViewById(R.id.text_oblique);
+
+            /*app:angle="LEFT_BOTTOM_TO_RIGHT_TOP"
+            app:basecolor="#2E3192"
+            app:endcolor="#FBB03B"
+            app:ending_slant_angle="0"
+            app:radius="18"
+            app:shadow="10"
+            app:startcolor="#D4145A"
+            app:starting_slant_angle="10"
+            app:type="linear_gradient"*/
+
+        switch (getArguments().getInt(ARG_SECTION_NUMBER)) {
+            case 1:
+                setup_1(obliqueView);
+                break;
+            case 2:
+                setup_2(obliqueView);
+                break;
+            case 3:
+                setup_1(obliqueView);
+                break;
+            case 4:
+                setup_2(obliqueView);
+                break;
+            case 5:
+                setup_1(obliqueView);
+                break;
+            default:
+
+                break;
+        }
+        return rootView;
+    }
+
+    private void setup_1(ObliqueView obliqueView) {
+        obliqueView.setType(Type.LINEAR_GRADIENT);
+        obliqueView.setStartColor(Color.parseColor("#D4145A"));
+        obliqueView.setEndColor(Color.parseColor("#FBB03B"));
+        obliqueView.setAngle(GradientAngle.LEFT_BOTTOM_TO_RIGHT_TOP);
+        obliqueView.setStartAngle(16);
+        obliqueView.setEndAngle(0);
+        obliqueView.setCornerRadius(15);
+        obliqueView.setShadow(10);
+    }
+
+    private void setup_2(ObliqueView obliqueView) {
+        obliqueView.setType(Type.IMAGE);
+        obliqueView.setEndAngle(10);
+        obliqueView.setStartAngle(10);
+        obliqueView.setCornerRadius(15);
+        obliqueView.setShadow(10);
+        obliqueView.setImageResource(R.drawable.pretty_1);
+    }
+}
